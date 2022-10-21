69988c9fd40a7aa1a1b7b0a5fee0926bd82d6115
chinder <chinder2022@163.com>
2022-10-21 11:39:46 +0800

    114

--- 本地题目/leetcode/editor/cn/[114]二叉树展开为链表.java
+++ 本地题目/leetcode/editor/cn/[114]二叉树展开为链表.java
@@ -61,8 +61,40 @@
  * }
  */
 class Solution {
+    /* method 2: ⽤「分解问题」的思维模式 */
+    // 思考过程：
+    // 1. 给出 flatten 函数的定义：
+    //      输⼊节点 root，然后 root 为根的二叉树就会被拉平为⼀条链表
+    //      void flatten(TreeNode root);
+    //
+    // 2. 有了这个函数定义，如何按题⽬要求把⼀棵树拉平成⼀条链表？
+    //      对于⼀个节点 x，可以执⾏以下流程：
+    //      i. 先利⽤ flatten(x.left) 和 flatten(x.right) 将 x 的左右⼦树拉平。
+    //      ii.将 x 的右⼦树接到左⼦树下⽅，然后将整个左⼦树作为右⼦树。
+    //
+    // 这就是递归的魅⼒，不容易说清楚 flatten 函数是怎么把左右⼦树拉平的，
+    // 但是只要知道 flatten 的定义如此并利⽤这个定义，让每⼀个节点做它该做的事情，然后flatten 函数就会按照定义⼯作。
     public void flatten(TreeNode root) {
+        // base case
+        if (root==null) return;
 
+        // 利⽤定义，把左右⼦树拉平
+        flatten(root.left);
+        flatten(root.right);
+
+        /* 后序遍历位置 */
+        // 1、左右⼦树已经被拉平成⼀条链表
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        // 2、将左⼦树作为右⼦树
+        root.left = null;
+        root.right = left;
+        // 3、将原先的右⼦树接到当前右⼦树的末端
+        TreeNode p = root;
+        while(p.right!=null){
+            p=p.right;
+        }
+        p.right = right;
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
